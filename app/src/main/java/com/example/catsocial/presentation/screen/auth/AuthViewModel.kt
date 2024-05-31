cec4792a76b71b940ce158f81374c4688dce321c
rfqagst <agustiarifqi@gmail.com>
2024-05-31 09:54:25 +0700

    Update Firebase Impl

--- app/src/main/java/com/example/catsocial/presentation/screen/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/catsocial/presentation/screen/auth/AuthViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.catsocial.presentation.screen.auth
 
 import androidx.lifecycle.ViewModel
+import com.example.catsocial.data.firebase.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class AuthViewModel : ViewModel() {
+class AuthViewModel @Inject constructor(
+    val repository: AuthRepository
+) : ViewModel() {
 }
